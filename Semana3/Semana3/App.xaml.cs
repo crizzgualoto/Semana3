20bc03f44e4ad9e9651b2febde1da5f553cce818
Usuario <Usuario@LAPTOP-A0CLRHHF>
2022-12-13 19:32:12 -0500

    Agregar archivos de proyecto.

---
+++ Semana3/Semana3/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            //navegar entre pestañas
+            MainPage = new NavigationPage(new Login());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Semana3/Semana3/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "Cristhian";
+            string contraseña = "12345";
+            
+            if(usuario==txtUsuario.Text && contraseña == txtContraseña.Text)
+            {
+                Navigation.PushAsync(new Registro(usuario));
+
+            }
+            else
+            {
+                DisplayAlert("alerta","USUARIO/CONTRASEÑA INCORRECTA","cerrar");
+
+            }
+            
+        }
+    }
+}
